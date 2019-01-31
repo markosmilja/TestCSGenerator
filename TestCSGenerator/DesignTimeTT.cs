1ccbcd2993bdd4417754c2ea579973f19d80dfee
Marko Smiljanic <marko.smiljanic@finallinetechnologies.com>
2019-01-31 17:02:15 +0100

    Changes

---
+++ TestCSGenerator/DesignTimeTT.cs
@@ -0,0 +1,10 @@
+﻿
+// This is generated code:
+class MyGeneratedClass {
+  // Generated code:
+  private int IntProp1 = 0;
+  // Generated code:
+  private int IntProp2 = 0;
+  // Generated code:
+  private int IntProp3 = 0;
+}
--- TestCSGenerator/Program.cs
+++ TestCSGenerator/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,9 @@ namespace TestCSGenerator
     {
         static void Main(string[] args)
         {
+            string applicationDir = Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().CodeBase).AbsolutePath) + "\\";
+            //File.WriteAllText(applicationDir + "Properties.txt", "E");
+            var protpertiaes = File.ReadAllLines(applicationDir + "Properties.txt");
         }
     }
 }
